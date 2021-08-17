ccc0f45c70a6de1a0225aa1bf6c59c65fe5ca07a
hash243 <63360680+hash243@users.noreply.github.com>
2021-08-17 22:06:17 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+* Navigate to ⚙ Settings -> Secrets -> New repository secret in Github
+* On the Name field enter **NGROK_AUTH_TOKEN**
+* On the Value field enter the Authtoken you get from https://dashboard.ngrok.com/auth/your-authtoken
+* Click Add secret
+* Then navigate to Action -> nmv -> Run workflow
+* Refresh the page and click nmv -> build
+* The URL for login and credential should display at the one before the last dropdown.
---
+++ run.sh
@@ -0,0 +1,33 @@
+#run.sh VNC_USER_PASSWORD VNC_PASSWORD NGROK_AUTH_TOKEN
+
+#stop indexing by spotlight
+sudo mdutil -i off -a
+
+#Create a new user account
+sudo dscl . -create /Users/user1
+sudo dscl . -create /Users/user1 UserShell /bin/bash
+sudo dscl . -create /Users/user1 RealName "user1"
+sudo dscl . -create /Users/user1 UniqueID 1001
+sudo dscl . -create /Users/user1 PrimaryGroupID 80
+sudo dscl . -create /Users/user1 NFSHomeDirectory /Users/vncuser
+sudo dscl . -passwd /Users/user1 $1
+sudo dscl . -passwd /Users/user1 $1
+sudo createhomedir -c -u user1 > /dev/null
+
+#VNC setup
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
+
+#VNC default pass change - https://pastebin.com/6sYbfuEd
+echo $2 | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
+
+#Restart VNC
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
+
+#Setup ngrok
+brew install --cask ngrok
+
+#Start ngrok by giving token and region
+ngrok authtoken $3
+ngrok tcp 5900 --region=ap &
